71e77df6c426cbfdebe85a6eb6625b44e15a9248
umaiibo <j.hnpn123@gmail.com>
2020-05-12 09:54:52 -0700

    created express app for server

---
+++ server.js
@@ -0,0 +1,24 @@
+var express = require("express");
+
+var PORT = process.env.PORT || 3000;
+
+var app = express();
+
+// Parse application body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("public"));
+
+// Set Handlebars.
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Start our server so that it can begin listening to client requests.
+app.listen(PORT, function() {
+    // Log (server-side) when our server has started
+    console.log("Server listening on: http://localhost:" + PORT);
+});
