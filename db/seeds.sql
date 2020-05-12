ba7820294f8f88a136c0c6885fcc5221fea876a5
umaiibo <j.hnpn123@gmail.com>
2020-05-12 09:50:55 -0700

    created folders for configuration and database

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ('Western Bacon Cheeseburger');
+INSERT INTO burgers (burger_name) VALUES ('Juicy Lucy');
+INSERT INTO burgers (burger_name) VALUES ('Classic');
