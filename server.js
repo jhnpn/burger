62e5f2f7b8c6cf55a7ab3af37d5c3f97b3eb09f8
umaiibo <j.hnpn123@gmail.com>
2020-05-12 22:12:34 -0700

    added connections to api routes and controllers files

--- server.js
+++ server.js
@@ -4,18 +4,23 @@ var PORT = process.env.PORT || 3000;
 
 var app = express();
 
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("public"));
+
 // Parse application body as JSON
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
-// Serve static content for the app from the "public" directory in the application directory.
-app.use(express.static("public"));
 
 // Set Handlebars.
 var exphbs = require("express-handlebars");
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
+
+var routes = require("./controllers/burgers_controllers");
+
+app.use(routes);
+
 
 // Start our server so that it can begin listening to client requests.
 app.listen(PORT, function() {
