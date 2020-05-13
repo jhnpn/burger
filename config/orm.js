5c471c33697af973d402c94b3f1c8d24dc4153df
umaiibo <j.hnpn123@gmail.com>
2020-05-12 22:10:50 -0700

    changed some things around within the update function

--- config/orm.js
+++ config/orm.js
@@ -1,4 +1,4 @@
-var connection = require("../config/connection.js");
+var connection = require("./connection.js");
 
 function printQuestionMarks(num) {
     var arr = [];
@@ -11,28 +11,16 @@ function printQuestionMarks(num) {
 }
 
 function objToSql(ob) {
+    // column1=value, column2=value2,...
     var arr = [];
 
-    // loop through the keys and push the key/value as a string int arr
     for (var key in ob) {
-        var value = ob[key];
-        // check to skip hidden properties
-        if (Object.hasOwnProperty.call(ob, key)) {
-            // if string with spaces, add quotations (Lana Del Grey => 'Lana Del Grey')
-            if (typeof value === "string" && value.indexOf(" ") >= 0) {
-                value = "'" + value + "'";
-            }
-            // e.g. {name: 'Lana Del Grey'} => ["name='Lana Del Grey'"]
-            // e.g. {sleepy: true} => ["sleepy=true"]
-            arr.push(key + "=" + value);
-        }
+        arr.push(key + "=" + ob[key]);
     }
 
-    // translate array of strings to a single comma-separated string
     return arr.toString();
 }
 
-// Object for all our SQL statement functions.
 var orm = {
     all: function(tableInput, cb) {
         var queryString = "SELECT * FROM " + tableInput + ";";
@@ -43,6 +31,8 @@ var orm = {
             cb(result);
         });
     },
+    // vals is an array of values that we want to save to cols
+    // cols are the columns we want to insert the values into
     create: function(table, cols, vals, cb) {
         var queryString = "INSERT INTO " + table;
 
@@ -59,11 +49,11 @@ var orm = {
             if (err) {
                 throw err;
             }
-
             cb(result);
         });
     },
-    // An example of objColVals would be {name: panther, sleepy: true}
+    // objColVals would be the columns and values that you want to update
+    // an example of objColVals would be {name: panther, sleepy: true}
     update: function(table, objColVals, condition, cb) {
         var queryString = "UPDATE " + table;
 
@@ -77,24 +67,9 @@ var orm = {
             if (err) {
                 throw err;
             }
-
-            cb(result);
-        });
-    },
-    delete: function(table, condition, cb) {
-        var queryString = "DELETE FROM " + table;
-        queryString += " WHERE ";
-        queryString += condition;
-
-        connection.query(queryString, function(err, result) {
-            if (err) {
-                throw err;
-            }
-
             cb(result);
         });
     }
 };
 
-// Export the orm object for the model
 module.exports = orm;
